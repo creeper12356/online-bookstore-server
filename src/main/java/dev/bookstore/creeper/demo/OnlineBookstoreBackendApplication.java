b562976f34f6d89e0cb167221f42b61b06428d56
creeper <creeperhjt@sjtu.edu.cn>
2024-11-10 20:30:02 +0800

    feat: call CalPriceService

--- src/main/java/dev/bookstore/creeper/demo/OnlineBookstoreBackendApplication.java
+++ src/main/java/dev/bookstore/creeper/demo/OnlineBookstoreBackendApplication.java
@@ -4,9 +4,19 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OnlineBookstoreBackendApplication {
+
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
